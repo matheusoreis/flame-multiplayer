3e6d82fa6426b56b38e76739700c0a857911f016
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-01-23 15:04:08 -0300

    Correção no packet

--- server/lib/net/protocol/packet.dart
+++ server/lib/net/protocol/packet.dart
@@ -1,3 +1,4 @@
+import 'package:server/core/player.dart';
 import 'package:server/net/buffers/reader.dart';
 import 'package:server/net/buffers/writer.dart';
 import 'package:server/net/protocol/packets/ping.dart';
@@ -9,7 +10,7 @@ abstract class Packet {
 
   void serialize(Writer writer);
   void deserialize(Reader reader);
-  void handle(int id);
+  void handle(Player player);
 }
 
 final Map<Headers, Packet Function()> packets = {
