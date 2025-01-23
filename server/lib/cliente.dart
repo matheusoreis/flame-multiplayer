37591b3418e679292890705f4e49b900f7263d80
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-01-23 15:05:53 -0300

    Cliente temporário para testes

---
+++ server/lib/cliente.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:server/net/buffers/writer.dart';
+
+void main() async {
+  final String serverUrl = 'ws://localhost:8080';
+
+  try {
+    final WebSocket socket = await WebSocket.connect(serverUrl);
+
+    print('Conectado ao servidor: $serverUrl');
+
+    final writer = Writer(2048);
+
+    writer.u16(0);
+    writer.string('Ping!');
+
+    socket.add(writer.getBuffer());
+    print('Pacote enviado: ${writer.getBuffer()}');
+
+    socket.listen(
+      (data) {
+        print('Recebido do servidor: $data');
+      },
+      onError: (error) {
+        print('Erro no WebSocket: $error');
+      },
+      onDone: () {
+        print('Conexão fechada pelo servidor');
+        print('Close Code: ${socket.closeCode}');
+        print('Close Reason: ${socket.closeReason}');
+      },
+    );
+  } catch (e) {
+    print('Erro ao conectar ao servidor: $e');
+  }
+}
