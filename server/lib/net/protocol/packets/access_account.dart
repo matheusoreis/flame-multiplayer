addb5e6fba35e98da0c46ad6adcb85f736a9160a
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-01-23 16:17:25 -0300

    Pacote de acesso, criação e remoção da conta

---
+++ server/lib/net/protocol/packets/access_account.dart
@@ -0,0 +1,36 @@
+import 'package:server/core/player.dart';
+import 'package:server/net/buffers/reader.dart';
+import 'package:server/net/buffers/writer.dart';
+import 'package:server/net/manager.dart';
+import 'package:server/net/protocol/packet.dart';
+import 'package:server/utils/services.dart';
+
+class AccessAccount implements Packet {
+  final Services _services;
+  late Manager _manager;
+
+  AccessAccount() : _services = Services() {
+    _manager = _services.get<Manager>();
+  }
+
+  @override
+  int header = Headers.accessAccount.index;
+  late String email;
+  late String password;
+
+  @override
+  void deserialize(Reader reader) {
+    email = reader.string();
+    password = reader.string();
+  }
+
+  @override
+  void handle(Player player) {
+    _manager.sendTo(player, this);
+  }
+
+  @override
+  void serialize(Writer writer) {
+    writer.u16(header);
+  }
+}
---
+++ server/lib/net/protocol/packets/delete_account.dart
@@ -0,0 +1,34 @@
+import 'package:server/core/player.dart';
+import 'package:server/net/buffers/reader.dart';
+import 'package:server/net/buffers/writer.dart';
+import 'package:server/net/manager.dart';
+import 'package:server/net/protocol/packet.dart';
+import 'package:server/utils/services.dart';
+
+class CreateAccount implements Packet {
+  final Services _services;
+  late Manager _manager;
+
+  CreateAccount() : _services = Services() {
+    _manager = _services.get<Manager>();
+  }
+
+  @override
+  int header = Headers.deleteAccount.index;
+  late String password;
+
+  @override
+  void deserialize(Reader reader) {
+    password = reader.string();
+  }
+
+  @override
+  void handle(Player player) {
+    _manager.sendTo(player, this);
+  }
+
+  @override
+  void serialize(Writer writer) {
+    writer.u16(header);
+  }
+}
